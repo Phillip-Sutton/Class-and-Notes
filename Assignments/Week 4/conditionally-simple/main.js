a879b7e7a6510c49f4faa68ae31140b1808a5ce6
Phillip-Sutton <148406483+Phillip-Sutton@users.noreply.github.com>
2024-05-14 17:59:53 -0500

    Update main.js

--- Assignments/Week 4/conditionally-simple/main.js
+++ Assignments/Week 4/conditionally-simple/main.js
@@ -1,6 +1,65 @@
 /******************************
  * YOUR CODE GOES RIGHT HERE! *
  ******************************/
+function isItLong(inputString) {
+  if (inputString.length > 20) {
+      return "That's a long string!";
+  }
+}
+function isItMedium(inputString) {
+  const length = inputString.length;
+  if (length >= 10 && length <= 20) {
+      return "That's a regular sized string!";
+  }
+}
+function isItShort(inputString) {
+  if (inputString.length < 10) {
+      return "That's a small string!";
+  } else {
+      return "That's not a small string!";
+  }
+}
+function howLongIsMyString(inputString) {
+  const length = inputString.length;
+  if (length > 20) {
+      return "That's a long string!";
+  } else if (length >= 10) {
+      return "That's a regular sized string!";
+  } else {
+      return "That's a small string!";
+  }
+}
+function personsHeight(name) {
+  const instructorHeights = {
+      "Shaq": 85,
+      "Zooey": 65,
+  };
+  if (name in instructorHeights) {
+      return `Height of ${name}: ${instructorHeights[name]} inches`;
+  } else {
+      return "I don't know everyone's height!";
+  }
+}
+const testString1 = "Tiny";
+const testString2 = "Reasonably long string";
+const testString3 = "This is a really long string!";
+
+console.log(isItLong(testString1));
+console.log(isItLong(testString2));
+
+console.log(isItMedium(testString1));
+console.log(isItMedium(testString2));
+console.log(isItMedium(testString3));
+
+console.log(isItShort(testString1));
+console.log(isItShort(testString2));
+
+console.log(howLongIsMyString(testString1));
+console.log(howLongIsMyString(testString2));
+
+console.log(personsHeight("Shaq"));
+console.log(personsHeight("Zooey"));
+console.log(personsHeight("John"));
 
 
 
