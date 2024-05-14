146bd0d82260971d2e2a39f5715703c8d15c1512
Phillip-Sutton <148406483+Phillip-Sutton@users.noreply.github.com>
2024-05-14 18:00:36 -0500

    Update main.js

--- Assignments/Week 4/fizzy/main.js
+++ Assignments/Week 4/fizzy/main.js
@@ -2,7 +2,17 @@
  * YOUR CODE GOES RIGHT HERE! *
  ******************************/
 
-
+for (let num = 1; num <= 100; num++) {
+  if (num % 3 === 0 && num % 5 === 0) {
+      console.log("FizzBuzz");
+  } else if (num % 3 === 0) {
+      console.log("Fizz");
+  } else if (num % 5 === 0) {
+      console.log("Buzz");
+  } else {
+      console.log(num);
+  }
+};
 
 
 /**********************************
